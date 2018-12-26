c6e5d1150e21e671a53bec38b578b3d234cb323d
Kevin <kevin@cerda.io>
2018-12-26 13:10:26 -0800

    Basic express server configured, listening on port 3000.

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.listen(PORT, err => {
+  err
+    ? console.err(`Error starting server on port ${PORT}: ${err}`)
+    : console.log(`Server is listening on port ${PORT}`);
+});
